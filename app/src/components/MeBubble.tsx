d2f277c04099a316ac6c28db5e286795d3c529b5
heygideon <47083178+heygideon@users.noreply.github.com>
2025-10-19 08:25:55 +0100

    fix: make text smaller when not typing

--- app/src/components/MeBubble.tsx
+++ app/src/components/MeBubble.tsx
@@ -29,16 +29,24 @@ export default function MeBubble({
     <motion.div
       animate={{ height: meHeights[typingState] }}
       transition={{ type: "spring", bounce: 0.5, duration: 0.7 }}
-      className="relative flex flex-none items-center justify-center-safe rounded-3xl bg-sky-600 px-4 pt-8 font-medium text-white"
+      className="relative flex flex-none items-center-safe justify-center-safe rounded-3xl bg-sky-600 px-4 py-3.5 font-medium text-white"
     >
       <p
         className={clsx(
-          "flex max-w-full flex-wrap justify-center gap-x-[5px] overflow-clip text-center",
-          typingState === "other" && "whitespace-nowrap opacity-50",
+          "flex max-w-full flex-wrap justify-center overflow-clip text-center",
+          typingState === "other"
+            ? "gap-x-[4.5px] whitespace-nowrap opacity-50"
+            : "gap-x-[5px]",
         )}
       >
         {message.split(" ").map((word, index) => (
-          <span key={index} className="text-lg whitespace-nowrap">
+          <span
+            key={index}
+            className={clsx(
+              "whitespace-nowrap",
+              typingState !== "other" && "text-lg",
+            )}
+          >
             {word.split("").map((char, charIndex) => (
               <span key={charIndex} className="animate-char-in inline-block">
                 {char}
@@ -47,12 +55,15 @@ export default function MeBubble({
           </span>
         ))}
       </p>
-      <div className="absolute inset-0 flex flex-col justify-center pt-8">
+      <div className="absolute inset-0 flex flex-col justify-center-safe py-3.5">
         <textarea
           value={message}
           onChange={(ev) => setMessage(ev.target.value)}
           onKeyDown={handleKeyDown}
-          className="field-sizing-content w-full resize-none overflow-clip px-[17px] text-center text-lg text-transparent caret-white outline-none"
+          className={clsx(
+            "field-sizing-content w-full resize-none overflow-clip px-[17px] text-center text-transparent caret-white outline-none",
+            typingState !== "other" && "text-lg",
+          )}
         />
       </div>
     </motion.div>
